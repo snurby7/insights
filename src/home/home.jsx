f11d59a14c9c7d73766e7c1dfa6f8fa7fc2e36e8
snurby7 <ryan.bruns7@gmail.com>
2018-11-20 20:49:24 -0500

    routing is a little different than angular that's for sure

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,45 +1,53 @@
 import React from 'react';
 import YnabService from '../ynab/ynab.service';
+import {Redirect} from 'react-router'
 
 function renderBudgetTile(props) {
-    return props.budgets.map(budget => {
-        return (
-            <li key={budget.id}>
-                <button onClick={() => props.handleClick(budget.id)}>{budget.name}</button>
-            </li>
-        )
-    })
+    return props
+        .budgets
+        .map(budget => <li key={budget.id}>
+            <button onClick={() => props.handleClick(budget.id)}>{budget.name}</button>
+        </li>);
 }
 
-
 class HomePage extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            budgets: []
+            budgets: [],
+            budgetId: null
         }
 
     }
     render() {
+        const {budgetId} = this.state;
+        if (budgetId !== null) {
+            // seems fishy, sure I'll be able to come back later and figure out how to correctly route.
+            const budgetRoute = `/budget/${budgetId}`
+            return <Redirect to={budgetRoute} push={true}/>
+        }
         return (
             <div>
                 <ol>
                     {renderBudgetTile({
                         budgets: this.state.budgets,
-                        handleClick: (budgetId) => {this.updateStateAndRoute(budgetId)}
+                        handleClick: (budgetId) => {
+                            this.updateStateAndRoute(budgetId)
+                        }
                     })}
                 </ol>
             </div>
-         );
+        );
     }
 
     componentDidMount() {
-        YnabService.getBudgets(this.state.api)
+        YnabService
+            .getBudgets(this.state.api)
             .then(response => this.setState({budgets: response.data.budgets}))
     }
 
     updateStateAndRoute = (budgetId) => {
-        this.props.onRoute(budgetId);
+        this.setState({budgetId});
     }
 }
 
