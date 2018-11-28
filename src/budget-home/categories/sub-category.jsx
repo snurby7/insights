b9cca3b4d4c204e467fee3b0158bb96ac777640c
snurby7 <ryan.bruns7@gmail.com>
2018-11-28 07:09:30 -0500

    get the biggest offender in a category

--- src/budget-home/categories/sub-category.jsx
+++ src/budget-home/categories/sub-category.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function displayCategoryItem(props) {
     return (
         <li key={props.id}>
-            {props.name} - ${props.activity / 1000}
+            {props.name} - <strong>${props.activity / 1000}</strong>
         </li>
     )
 }
@@ -12,19 +12,37 @@ class SubCategory extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            subCategories: props.subCategories
+            subCategories: props.subCategories,
+            biggestSpendingCategory: this.getBiggestSpendingCategory(props.subCategories)
         }
     }
 
     render() {
         return (
             <div>
-                The biggest offender is TODO
+                The biggest offender is {this.getBiggestOffender()}
                 <ol>
                     {this.state.subCategories.map(x => displayCategoryItem(x))}
                 </ol>
             </div>
         );
+    }
+
+    getBiggestSpendingCategory(subCategories) {
+        let biggestSpendingCategory = null;
+        subCategories.forEach(subCategory => {
+            if(!biggestSpendingCategory) {
+                biggestSpendingCategory = subCategory;
+            } else if(subCategory.activity < biggestSpendingCategory.activity){
+                biggestSpendingCategory = subCategory;
+            }
+        });
+        return biggestSpendingCategory
+    }
+
+    getBiggestOffender() {
+        if(!this.state.biggestSpendingCategory) return '';
+        return this.state.biggestSpendingCategory.name;
     }
 }
 
