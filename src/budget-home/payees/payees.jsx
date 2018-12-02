3cc79d5fb6539df69670ca4591b67570d9b8f681
snurby7 <ryan.bruns7@gmail.com>
2018-12-02 08:41:42 -0500

    add a filter to the payees.

--- src/budget-home/payees/payees.jsx
+++ src/budget-home/payees/payees.jsx
@@ -9,15 +9,42 @@ class Payees extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            payees: []
+            masterPayees: [],
+            payees: [],
+            value: ''
         }
     }
+
+    handleInputChange(event) {
+        const inputValue = event.target.value;
+        if (inputValue === "") {
+            this.setState({payees: this.state.masterPayees})
+        } else {
+            this.setState({
+                payees: this
+                    .state
+                    .masterPayees
+                    .filter(x => x.name.toLowerCase().includes(inputValue.toLowerCase()))
+            })
+        }
+        this.setState({value: event.target.value})
+    }
+
     render() {
         return (
             <div>
                 <h2>Payees</h2>
+                <label>Name:
+                    <input
+                        type="text"
+                        value={this.state.value}
+                        onChange={(event) => this.handleInputChange(event)}/>
+                </label>
                 <ol>
-                    {this.state.payees.map(x => renderPayee(x))}
+                    {this
+                        .state
+                        .payees
+                        .map(x => renderPayee(x))}
                 </ol>
             </div>
         );
@@ -26,7 +53,7 @@ class Payees extends React.Component {
     componentDidMount() {
         YnabService
             .getPayees(this.props.budgetId)
-            .then(response => this.setState({payees: response.data.payees}))
+            .then(response => this.setState({payees: response.data.payees, masterPayees: response.data.payees}))
     }
 }
 
