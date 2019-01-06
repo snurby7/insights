36145a697f34432213de2a6280e065927480319c
snurby7 <ryan.bruns7@gmail.com>
2019-01-06 10:10:47 -0500

    removed another route combined them into one.

--- client/src/admin/admin.jsx
+++ client/src/admin/admin.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import GridDisplay from "../common/grid-display";
-import RoutingButton from "../common/routing-button";
 import AdminAgent from "../agents/admin-agent";
-
+import RbButton from "../common/rb-button";
+import UserManagement from "./management/user-management";
 // TODO make this look less bad.
 // TODO make this behave like the budget home does now and it all uses the same page
+// TODO make this have some sort of props.context or this.context so I don't need to select a budgetId every time
 
 class AdminPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       budgets: [],
-      selectedBudget: null
+      selectedBudget: null,
+      selectedSection: null
     };
+  }
+
+  getDisplayToggles() {
+    // TODO make these an enum instead of some number
+    return [
+      {
+        displayName: "Data Updaters",
+        onClick: () => this.setState({ selectedSection: 1 })
+      },
+      {
+        displayName: "User Management",
+        onClick: () => this.setState({ selectedSection: 2 })
+      }
+    ];
   }
 
   getButtonsToRender() {
@@ -35,14 +51,16 @@ class AdminPage extends React.Component {
         id: 3,
         cardTitle: "Transactions",
         subTitles: ["Refresh all transactions from YNAB and store results"],
-        onClick: () => AdminAgent.udateTransactions(this.state.selectedBudget.id),
+        onClick: () =>
+          AdminAgent.udateTransactions(this.state.selectedBudget.id),
         buttonText: "Update"
       },
       {
         id: 4,
         cardTitle: "Categories",
         subTitles: ["Refresh all categories from YNAB and store results"],
-        onClick: () => AdminAgent.updateCategories(this.state.selectedBudget.id),
+        onClick: () =>
+          AdminAgent.updateCategories(this.state.selectedBudget.id),
         buttonText: "Update"
       },
       {
@@ -75,27 +93,34 @@ class AdminPage extends React.Component {
   }
 
   render() {
-    const selectedBudget = this.state.selectedBudget;
+    const { selectedBudget, selectedSection } = this.state;
     const { id, name } = selectedBudget || {};
-    const userDisplayData = {
-      route: selectedBudget ? `/admin/${id}/users` : null,
-      displayName: `Manage users for ${name} `
-    };
     return (
       <div>
         <div>
           <GridDisplay displayData={this.state.budgets} />
         </div>
         <div>
-          {/* TODO make this a nice transition when it shows up */}
           {selectedBudget && (
-            <div>
+            <React.Fragment>
+              {this.getDisplayToggles().map((x, index) => (
+                <RbButton key={index} displayData={x} />
+              ))}
               <hr />
-              <RoutingButton displayData={userDisplayData} />
-              <h3>Refresh Options for {name}</h3>
-              <GridDisplay displayData={this.getButtonsToRender()} />
-            </div>
+            </React.Fragment>
           )}
+        </div>
+        <div>
+          {/* TODO make this a nice transition when it shows up */}
+          <div>
+            {selectedSection === 1 && (
+              <React.Fragment>
+                <h3>Refresh Options for {name}</h3>
+                <GridDisplay displayData={this.getButtonsToRender()} />
+              </React.Fragment>
+            )}
+            {selectedSection === 2 && <UserManagement budgetId={id} />}
+          </div>
         </div>
       </div>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -8,13 +8,11 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import AdminPage from './admin/admin';
 import BudgetHome from './budget-home/budget-home';
 import HomePage from './home/homepage';
-import UserManagement from './admin/management/user-management';
 import YnabAppBar from './common/ynab-app-bar';
 
 const AdminRoute = () => <AdminPage/>
 const BudgetHomeRoute = ({match}) => <BudgetHome budgetId={match.params.budgetId}/>;
 const HomePageRoute = () => <HomePage/>
-const UserManagementRoute = ({match}) => <UserManagement budgetId={match.params.budgetId}/>;
 
 // TODO footer component with key metrics, short and sweet high level.
 
@@ -29,7 +27,6 @@ class InsightApp extends React.Component {
             <Switch>
               <Route exact path="/" component={HomePageRoute}/>
               <Route exact path="/admin" component={AdminRoute}/>
-              <Route exact path="/admin/:budgetId/users" component={UserManagementRoute}/>
               <Route exact path="/budget/:budgetId" component={BudgetHomeRoute}/>
             </Switch>
           </div>
