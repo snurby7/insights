01e586cbe3af8d6ceb1e6f5d776972785e606f1a
snurby7 <ryan.bruns7@gmail.com>
2019-02-11 21:23:09 -0500

    use session storage as a fallback to store the budgetId  #21

--- client/src/reducers/SiteReducer.tsx
+++ client/src/reducers/SiteReducer.tsx
@@ -3,8 +3,13 @@ import { IReducerAction } from '../contracts/reducer-action.interface';
 import { ISiteState } from '../contracts/site-state.interface';
 
 function siteState(state: ISiteState = {}, action: IReducerAction) {
+  const sessionStorageBudgetId = sessionStorage.getItem('budgetId');
+  if (sessionStorageBudgetId) {
+    state.budgetId = sessionStorageBudgetId as string;
+  }
   switch (action.type) {
     case SiteActions.UPDATE_SELECTED_BUDGET:
+      sessionStorage.setItem('budgetId', action.payload.budgetId);
       return Object.assign({}, state, { budgetId: action.payload.budgetId });
     default:
       return state;
