589770241d8ef87a5076a4fee4ce95d24a8081ec
snurby7 <ryan.bruns7@gmail.com>
2019-03-04 13:29:58 -0500

    Add Dx Grid

--- client/src/data-views/accounts/view/AccountView.tsx
+++ client/src/data-views/accounts/view/AccountView.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import YnabAgent from '../../../agents/ynab-agent';
+import DxReactGridWrapper from '../../../grid/DxReactGridWrapper';
 import { IAccountViewProps } from './AccountViewProps.interface';
 
 export default class AccountView extends Component<IAccountViewProps> {
@@ -12,6 +13,7 @@ export default class AccountView extends Component<IAccountViewProps> {
   };
 
   public render() {
-    return <div>words</div>;
+    return <DxReactGridWrapper />;
+    // return <div>words</div>;
   }
 }
