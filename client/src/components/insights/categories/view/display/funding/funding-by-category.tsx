bb16e884805baceb9bf65765074b52512cbd20a5
snurby7 <ryan.bruns7@gmail.com>
2019-04-17 07:10:30 -0400

    some new components for viewing

---
+++ client/src/components/insights/categories/view/display/funding/funding-by-category.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import { ICategoryComponentProps } from '../contracts';
+
+export default class FundingByCategory extends Component<ICategoryComponentProps> {
+  public render() {
+    return <div>Funding by FundingByCategory</div>;
+  }
+}
---
+++ client/src/components/insights/categories/view/display/spending-report/spending-reporty-by-category.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import { ICategoryComponentProps } from '../contracts';
+
+export default class SpendingReportByCategory extends Component<ICategoryComponentProps> {
+  public render() {
+    return <div>Spending report by category</div>;
+  }
+}
